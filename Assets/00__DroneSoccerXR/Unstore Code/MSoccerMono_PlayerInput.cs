172fcca7accdc1047c2f8e9208839e09b8eb5f90
Éloi Strée <eloistree@gmail.com>
2024-06-01 15:13:47 +0200

    Save: 06/01/2024 3:13 PM

---
+++ Assets/00__DroneSoccerXR/Unstore Code/MSoccerMono_PlayerInput.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MSoccerMono_PlayerInput : NetworkBehaviour
+{
+
+    [Header("Client Information")]
+    public float m_rotateHorizontalPercent;
+    public float m_moveDownUpPercent;
+    public float m_moveLeftRightPercent;
+    public float m_moveBackForwardPercent;
+
+
+    public float m_localRotateHorizontalPercent;
+    public float m_localMoveDownUpPercent;
+    public float m_localMoveLeftRightPercent;
+    public float m_localMoveBackForwardPercent;
+
+
+    //4 * 4 bytes 16
+    //0-99
+    //9988776600
+    //2147483647
+
+    public void SetLocalRotateHorizontalPercent(float value) { m_localRotateHorizontalPercent = value; }
+    public void SetLocalMoveDownUpPercent(float value) { m_localMoveDownUpPercent=value; }
+    public void SetLocalMoveLeftRightPercent(float value) { m_localMoveLeftRightPercent=value; }
+    public void SetLocalMoveBackForwardPercent (float value) { m_localMoveBackForwardPercent = value; }
+
+
+
+    public void PushCurrentLocalValueToServer() {
+        if (!isOwned)
+            return;
+        PushCurrentJoystickValueToServer(m_localRotateHorizontalPercent, m_localMoveDownUpPercent, m_localMoveLeftRightPercent, m_localMoveBackForwardPercent);
+    }
+
+    public void PushCurrentJoystickValueToServer(float rotateHorizontalPercent,
+        float moveDownUpPercent,
+        float moveLeftRightPercent,
+        float moveBackForwardPercent) {
+        if (!isOwned)
+            return;
+        CmdPushInputPlayerToServer(rotateHorizontalPercent, moveDownUpPercent, moveLeftRightPercent, moveBackForwardPercent);
+    }
+
+    [Command]
+    void CmdPushInputPlayerToServer(
+        float rotateHorizontalPercent,
+        float moveDownUpPercent,
+        float moveLeftRightPercent,
+        float moveBackForwardPercent
+        ) {
+        m_rotateHorizontalPercent= rotateHorizontalPercent;
+        m_moveDownUpPercent= moveDownUpPercent;
+        m_moveLeftRightPercent= moveLeftRightPercent;
+        m_moveBackForwardPercent= moveBackForwardPercent;
+
+    }
+}

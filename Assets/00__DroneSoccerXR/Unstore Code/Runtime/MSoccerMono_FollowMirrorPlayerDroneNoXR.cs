105d186b85b5a8d04fe3097e46fcc8c1e26b6cc7
Eloi Stree <eloistree@gmail.com>
2024-06-20 20:44:19 +0200

    Save: 06/20/2024 8:44 PM

--- Assets/00__DroneSoccerXR/Unstore Code/Runtime/MSoccerMono_FollowMirrorPlayerDroneNoXR.cs
+++ Assets/00__DroneSoccerXR/Unstore Code/Runtime/MSoccerMono_FollowMirrorPlayerDroneNoXR.cs
@@ -7,24 +7,37 @@ public class MSoccerMono_FollowMirrorPlayerDroneNoXR : MonoBehaviour
 
     public Transform m_whatToMove;
     public Transform m_whatToFollow;
-    public float m_lerpPower=1;
+    public float m_lerpPower = 1;
+    public float m_lerpPowerRotation = 0.85f;
 
+    public bool m_useLateUpdate = false;
 
 
     private void Update()
     {
-        if (m_whatToFollow==null &&  MSoccerMono_PlayerInput.PlayerInstanceInScene != null) {
+        if(!m_useLateUpdate)
+            UpdateLerp();
+    }
+    public void LateUpdate()
+    {
+        if (m_useLateUpdate)
+            UpdateLerp();
+    }
+
+    private void UpdateLerp()
+    {
+        if (m_whatToFollow == null && MSoccerMono_PlayerInput.PlayerInstanceInScene != null)
+        {
 
             Rigidbody droneRig = MSoccerMono_PlayerInput.PlayerInstanceInScene.GetComponentInChildren<Rigidbody>();
-            if(droneRig!=null)
+            if (droneRig != null)
                 m_whatToFollow = droneRig.transform;
         }
 
         if (m_whatToFollow != null)
         {
             m_whatToMove.position = Vector3.Lerp(m_whatToFollow.position, m_whatToMove.position, Time.deltaTime * m_lerpPower);
-            m_whatToMove.rotation = Quaternion.Lerp(m_whatToFollow.rotation, m_whatToMove.rotation, Time.deltaTime * m_lerpPower);
+            m_whatToMove.rotation = Quaternion.Lerp(m_whatToFollow.rotation, m_whatToMove.rotation, Time.deltaTime * m_lerpPowerRotation);
         }
     }
-
 }
